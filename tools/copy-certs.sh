08863d8181a8f1f1ddc66e49e767e637f031c90f
Anton Shvets <shvets@corp.satel.org>
2021-07-08 21:24:54 +0200

    copy certs

---
+++ tools/copy-certs.sh
@@ -0,0 +1,5 @@
+mkdir cert
+sudo cp /etc/ssl/certs/ssl-cert-snakeoil.pem certs/ssl-cert-snakeoil.pem
+sudo cp /etc/ssl/private/ssl-cert-snakeoil.key certs/ssl-cert-snakeoil.key
+sudo chmod 666 cert -R
+sudo chmod a+x certs
